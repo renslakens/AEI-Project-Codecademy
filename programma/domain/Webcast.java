6f890afc868ca5f31d7b469c6c72b2d185795a45
Rens Lakens <rens@lakens.org>
2022-01-09 19:32:18 +0100

    added Webcast in domain package

---
+++ programma/domain/Webcast.java
@@ -0,0 +1,95 @@
+package programma.domain;
+
+import java.util.Date;
+
+public class Webcast {
+    // Attributes
+    private String titel;
+    private String beschrijving;
+    private String sprekerNaam;
+    private String organisatieNaam;
+    private int tijdsduur;
+    private Date publicatieDatum;
+    private String url;
+
+    // Constructor
+    public Webcast(String titel, String beschrijving, String sprekerNaam, String organisatieNaam, int tijdsduur, Date publicatieDatum, String url) {
+        this.titel = titel;
+        this.beschrijving = beschrijving;
+        this.sprekerNaam = sprekerNaam;
+        this.organisatieNaam = organisatieNaam;
+        this.tijdsduur = tijdsduur;
+        this.publicatieDatum = publicatieDatum;
+        this.url = url;
+    }
+
+    // Getters and Setters
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public String getBeschrijving() {
+        return beschrijving;
+    }
+
+    public void setBeschrijving(String beschrijving) {
+        this.beschrijving = beschrijving;
+    }
+
+    public String getSprekerNaam() {
+        return sprekerNaam;
+    }
+
+    public void setSprekerNaam(String sprekerNaam) {
+        this.sprekerNaam = sprekerNaam;
+    }
+
+    public String getOrganisatieNaam() {
+        return organisatieNaam;
+    }
+
+    public void setOrganisatieNaam(String organisatieNaam) {
+        this.organisatieNaam = organisatieNaam;
+    }
+
+    public int getTijdsduur() {
+        return tijdsduur;
+    }
+
+    public void setTijdsduur(int tijdsduur) {
+        this.tijdsduur = tijdsduur;
+    }
+
+    public Date getPublicatieDatum() {
+        return publicatieDatum;
+    }
+
+    public void setPublicatieDatum(Date publicatieDatum) {
+        this.publicatieDatum = publicatieDatum;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return
+                " Titel='" + getTitel() + "'" +
+                        ", description='" + getBeschrijving() + "'" +
+                        ", name speaker='" + getSprekerNaam() + "'" +
+                        ", org. name='" + getOrganisatieNaam() + "'" +
+                        ", time='" + getTijdsduur() + "'" +
+                        ", publication date='" + getPublicatieDatum() + "'" +
+                        ", url='" + getUrl() + "'";
+    }
+
+}
